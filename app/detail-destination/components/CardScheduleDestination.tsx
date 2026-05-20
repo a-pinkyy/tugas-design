e9ac71e5e0828a9126ae786bba4f40ad1dec992d
Avril <avrilzeltha714@gmail.com>
2026-05-20 22:30:55 +0800

    update kode

--- app/detail-destination/components/CardScheduleDestination.tsx
+++ app/detail-destination/components/CardScheduleDestination.tsx
@@ -1,44 +1,36 @@
 export default function CardScheduleDestination() {
   return (
     <div className="bg-white rounded-xl shadow-md mt-10 p-6 flex justify-between hover:shadow-xl hover:-translate-y-1 duration-300">
-
       <div className="w-[38%]">
         <div className="flex gap-3 items-center">
           <div className="flex">
             <div className="w-7 h-7 bg-red-100 rounded-full text-[10px] flex items-center justify-center">
               Batik
             </div>
-
             <div className="w-7 h-7 bg-red-100 rounded-full text-[10px] flex items-center justify-center -ml-2">
               Lion
             </div>
           </div>
-
           <h1 className="font-bold text-lg">
             Batik Air Indonesia + Lion Air
           </h1>
-
           <p className="text-lg">🧳 🍴</p>
         </div>
-
         <p className="bg-gray-100 inline-block mt-16 px-2 py-1 rounded text-sm">
           Tidak bisa reschedule & refund
         </p>
       </div>
-
       <div className="flex gap-7 w-[28%]">
         <div>
           <h1 className="text-3xl font-bold">14:55</h1>
           <p className="text-base text-gray-500 mt-1">BPN</p>
         </div>
-
         <div className="text-center text-gray-500 mt-1">
           <p className="text-base">6j 25m</p>
           <div className="border-t w-24 my-2"></div>
           <p className="text-base">1 transit</p>
         </div>
-
-        <div>
+       <div>
           <h1 className="text-3xl font-bold">20:20</h1>
           <p className="text-base text-gray-500 mt-1">KNO</p>
         </div>
